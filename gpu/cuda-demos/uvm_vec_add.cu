b704b8d13ba7359c72a4f676318edb40585a223e
Hao Wang <pkuwangh@gmail.com>
2024-01-21 00:14:14 -0800

    [gpu] add support for other cuda allocators for uvm test

--- gpu/cuda-demos/uvm_vec_add.cu
+++ gpu/cuda-demos/uvm_vec_add.cu
@@ -1,7 +1,13 @@
 #include <cmath>
 #include <cstdint>
+#include <cstdlib>
 #include <iostream>
+#include <set>
+#include <string>
+#include <boost/program_options.hpp>
 #include <cuda.h>
+
+namespace po = boost::program_options;
 
 // Kernel function to add the elements of two arrays
 __global__ void add(size_t n, float *x, float *y)
@@ -14,13 +20,31 @@ __global__ void add(size_t n, float *x, float *y)
     }
 }
 
-int main(void)
+int benchmark(const std::string &allocator)
 {
     size_t N = (size_t)(2) << 30; // 2G x 4B x 2
     float *x, *y;
 
-    cudaMallocManaged(&x, N * sizeof(float));
-    cudaMallocManaged(&y, N * sizeof(float));
+    if (allocator == "cudaMallocManaged")
+    {
+        cudaMallocManaged(&x, N * sizeof(float));
+        cudaMallocManaged(&y, N * sizeof(float));
+    }
+    else if (allocator == "cudaMallocHost")
+    {
+        cudaMallocHost(&x, N * sizeof(float));
+        cudaMallocHost(&y, N * sizeof(float));
+    }
+    else if (allocator == "malloc")
+    {
+        x = (float *)(malloc(N * sizeof(float)));
+        y = (float *)(malloc(N * sizeof(float)));
+    }
+    else
+    {
+        std::cout << "Unknown allocator " << allocator << std::endl;
+        return 1;
+    }
 
     for (size_t i = 0; i < N; ++i)
     {
@@ -33,7 +57,7 @@ int main(void)
     cudaEventCreate(&ckpt2);
     cudaEventCreate(&ckpt3);
 
-    std::cout << "Initialized " << N << " elements, GPU kernel start" << std::endl;
+    std::cout << "Initialized " << N << " elements using " << allocator << std::endl;
 
     size_t blockSize = 1024;
     size_t numBlocks = (N + blockSize - 1) / blockSize;
@@ -43,7 +67,8 @@ int main(void)
     add<<<numBlocks, blockSize>>>(N, x, y);
     cudaEventRecord(ckpt2);
 
-    for (int k = 0; k < 100; ++k) {
+    for (int k = 0; k < 100; ++k)
+    {
         add<<<numBlocks, blockSize>>>(N, x, y);
     }
     cudaEventRecord(ckpt3);
@@ -51,9 +76,9 @@ int main(void)
     cudaDeviceSynchronize();
     float elapsed = 0;
     cudaEventElapsedTime(&elapsed, ckpt1, ckpt2);
-    std::cout << "Elapsed time " << elapsed / 1.0e3 << " seconds - initial" << std::endl;
+    std::cout << "Elapsed time " << int(elapsed) << " mili-seconds - initial" << std::endl;
     cudaEventElapsedTime(&elapsed, ckpt2, ckpt3);
-    std::cout << "Elapsed time " << elapsed / 1.0e5 << " seconds - warmedup" << std::endl;
+    std::cout << "Elapsed time " << int(elapsed / 100) << " mili-seconds - warmedup" << std::endl;
 
     float maxError = 0.0f;
     for (size_t i = 0; i < N; i++)
@@ -65,8 +90,69 @@ int main(void)
     cudaEventDestroy(ckpt2);
     cudaEventDestroy(ckpt3);
 
-    cudaFree(x);
-    cudaFree(y);
-
+    if (allocator == "malloc")
+    {
+        free(x);
+        free(y);
+    }
+    else
+    {
+        cudaFree(x);
+        cudaFree(y);
+    }
     return int(maxError);
+}
+
+int main(int argc, char **argv)
+{
+    std::set<std::string> avail_allocators({"cudaMallocHost",
+                                            "cuMemCreate",
+                                            "cudaMallocManaged",
+                                            "malloc"});
+    std::string allocator;
+    po::options_description all_opts("uvm_vec_add CLI");
+    all_opts.add_options()("help,h", "Help message");
+    all_opts.add_options()("list,l", "list available allocator options");
+    all_opts.add_options()("allocator,a", po::value<std::string>(&allocator)->default_value("all"), "Which memory allocation API to use");
+
+    po::variables_map vm;
+    try
+    {
+        po::store(po::parse_command_line(argc, argv, all_opts), vm);
+        po::notify(vm);
+    }
+    catch (...)
+    {
+        std::cout << "Error: Invalid Arguments " << std::endl;
+        std::cout << all_opts << std::endl;
+        return 1;
+    }
+    if (vm.count("help"))
+    {
+        std::cout << all_opts << std::endl;
+        return 0;
+    }
+    if (vm.count("list"))
+    {
+        for (const std::string &x : avail_allocators)
+        {
+            std::cout << x << std::endl;
+        }
+        return 0;
+    }
+
+    int totalError = 0;
+    if (allocator == "all")
+    {
+        for (const std::string &x : avail_allocators)
+        {
+            totalError += benchmark(x);
+        }
+    }
+    else
+    {
+        totalError += benchmark(allocator);
+    }
+
+    return int(totalError);
 }
