49c1ab5c46a1129daf8196d13cd32585b11222db
Hao Wang <haowan@nvidia.com>
2024-02-28 22:19:03 -0800

    [redis-store] improve redis-store logging

--- mlsys/dist-process/demo-process-group/redis_store.py
+++ mlsys/dist-process/demo-process-group/redis_store.py
@@ -5,39 +5,43 @@ import os
 import redis
 import subprocess
 import time
-from datetime import timedelta
+from datetime import datetime, timedelta
 from torch.distributed import Store
-from torch._C._distributed_c10d import _DEFAULT_PG_TIMEOUT, _DEFAULT_PG_NCCL_TIMEOUT
 from typing import List, Optional
 
 
 class RedisStore(Store):
     def __init__(
-        self, host: str, port: int, is_server: bool, wait_interval: float = 0.1
+        self,
+        host: str,
+        port: int,
+        is_server: bool,
+        timeout: timedelta = timedelta(seconds=1200),
+        wait_interval: float = 0.1,
+        verbose: bool = False,
     ):
         # call base class constructor
-        super(RedisStore, self).__init__()
+        super().__init__()
+        assert host != "" and port > 0
         if is_server:
-            print("Starting Redis server", flush=True)
             curr_dir = os.path.dirname(os.path.abspath(__file__))
             redis_dir = os.path.join(curr_dir, "../../services/redis")
             redis_bin = os.path.join(redis_dir, "redis/src/redis-server")
             redis_conf = os.path.join(redis_dir, "redis.conf")
+            print(f"Starting Redis server using {redis_bin} {redis_conf}")
             proc = subprocess.Popen(
-                [redis_bin, redis_conf],
-                stdout=None,
-                stderr=None,
+                [redis_bin, redis_conf, "--port", str(port)],
+                stdout=subprocess.DEVNULL,
+                stderr=subprocess.DEVNULL,
                 shell=False,
             )
             atexit.register(proc.terminate)
-        if isinstance(_DEFAULT_PG_NCCL_TIMEOUT, timedelta):
-            self._timeout = _DEFAULT_PG_NCCL_TIMEOUT
-        elif isinstance(_DEFAULT_PG_TIMEOUT, timedelta):
-            self._timeout = _DEFAULT_PG_TIMEOUT
-        else:
-            self._timeout = timedelta(seconds=1200)
+            time.sleep(1)
+        self._timeout = timeout
         self._wait_interval = wait_interval
-        print(f"Connecting to Redis server at {host}:{port}", flush=True)
+        self._verbose = verbose
+        if self._verbose:
+            print(f"Connecting to Redis server at {host}:{port} at {datetime.now()}")
         self._redis = redis.Redis(host=host, port=port, db=0)
         curr_time = time.time()
         while True:
@@ -45,28 +49,36 @@ class RedisStore(Store):
                 self._redis.ping()
                 break
             except redis.exceptions.ConnectionError:
-                if time.time() - curr_time > 60:
+                if self._verbose:
+                    print(f"Failed to ping Redis server at {host}:{port}, retrying...")
+                if time.time() - curr_time > self._timeout.total_seconds():
                     raise TimeoutError("Timeout connecting to Redis server")
                 time.sleep(1)
+        if self._verbose:
+            print(f"Connected to Redis server at {datetime.now()}")
 
     def dump_keys(self):
         all_keys = self._redis.keys()
         print(f"all keys: {','.join([k.decode('utf-8') for k in all_keys])}")
 
     def set(self, key: str, value: str):
-        print(f"setting key={key} value={value}")
+        if self._verbose:
+            print(f"Setting key {key} to value {value}")
         self._redis.set(key, value)
 
     def get(self, key: str) -> bytes:
-        print(f"reading key={key}")
-        retries = [0, 1, self._timeout.total_seconds()]
-        for retry_interval in retries:
-            if retry_interval > 0:
-                time.sleep(retry_interval)
+        if self._verbose:
+            print(f"Getting key {key}")
+        start_time = time.time()
+        value = b""
+        while True:
             value = self._redis.get(key)
             if value is not None:
-                return value
-        raise KeyError(f"Key {key} not found")
+                break
+            if time.time() - start_time > self._timeout.total_seconds():
+                raise TimeoutError(f"Timeout getting key {key}")
+            time.sleep(self._wait_interval)
+        return value
 
     def add(self, key: str, value: int) -> int:
         return self._redis.incrby(key, value)
@@ -102,7 +114,8 @@ class RedisStore(Store):
         self._timeout = timeout
 
     def wait(self, keys: List[str], timeout: Optional[timedelta]=None):
-        print(f"Waiting for keys {keys}", flush=True)
+        if self._verbose:
+            print(f"Waiting for keys {keys}")
         eff_timeout = self._timeout if timeout is None else timeout
         start_time = time.time()
         pipe = self._redis.pipeline()
@@ -119,8 +132,3 @@ class RedisStore(Store):
             if (curr_time - start_time) > eff_timeout.total_seconds():
                 raise TimeoutError(f"Timeout waiting for keys {remaining_keys}")
             time.sleep(self._wait_interval)
-
-
-if __name__ == "__main__":
-    print(_DEFAULT_PG_NCCL_TIMEOUT)
-    print(_DEFAULT_PG_TIMEOUT)
