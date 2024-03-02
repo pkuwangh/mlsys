3308cdaf2eb53aab07e5d42142a986a96783f29a
Hao Wang <haowan@nvidia.com>
2024-03-01 21:55:24 -0800

    [redis-store] add status for redis-store client

--- mlsys/dist-process/demo-process-group/redis_store.py
+++ mlsys/dist-process/demo-process-group/redis_store.py
@@ -10,40 +10,81 @@ from torch.distributed import Store
 from typing import List, Optional, overload
 
 
+class RedisStoreStats:
+    def __init__(self):
+        self.req_type = ["get", "set", "add", "compare", "delete", "wait"]
+        self.requests = {
+            "count": {x: 0 for x in self.req_type},
+            "total_delay": {x: 0 for x in self.req_type},
+            "max_delay": {x: 0 for x in self.req_type},
+            "max_retry": {x: 0 for x in self.req_type},
+        }
+        self.startup_time = 0
+
+    def reset(self):
+        for req in self.req_type:
+            self.requests["count"][req] = 0
+            self.requests["total_delay"][req] = 0
+            self.requests["max_delay"][req] = 0
+            self.requests["max_retry"][req] = 0
+
+    def dump(self):
+        print(f"Startup time: {self.startup_time}")
+        for req in self.req_type:
+            if self.requests["count"][req] == 0:
+                avg_delay = 0
+            else:
+                avg_delay = (self.requests["total_delay"][req] /
+                             self.requests["count"][req])
+            print(f"{req}: count={self.requests['count'][req]} "
+                  f"avg_delay={avg_delay*1000:.2f}ms "
+                  f"max_delay={self.requests['max_delay'][req]*1000:.2f}ms "
+                  f"max_retry={self.requests['max_retry'][req]}")
+
+    def add_delay(self, req: str, delay: float, retry: int=0):
+        self.requests["count"][req] += 1
+        self.requests["total_delay"][req] += delay
+        self.requests["max_delay"][req] = max(self.requests["max_delay"][req], delay)
+        self.requests["max_retry"][req] = max(self.requests["max_retry"][req], retry)
+
+
 class RedisStore(Store):
     def __init__(
         self,
-        host: str,
-        port: int,
         is_server: bool,
         timeout: timedelta = timedelta(seconds=1200),
         wait_interval: float = 0.1,
         verbose: bool = False,
     ):
+        start_time = time.time()
         # call base class constructor
         super().__init__()
-        assert host != "" and port > 0
+        host = os.environ.get("MASTER_ADDR", "")
+        port = os.environ.get("MASTER_PORT", "")
+        if host == "" or port == "":
+            raise ValueError("MASTER_ADDR and MASTER_PORT must be set")
         if is_server:
             curr_dir = os.path.dirname(os.path.abspath(__file__))
-            redis_dir = os.path.join(curr_dir, "../../services/redis")
-            redis_bin = os.path.join(redis_dir, "redis/src/redis-server")
-            redis_conf = os.path.join(redis_dir, "redis.conf")
-            print(f"Starting Redis server using {redis_bin} {redis_conf}")
+            redis_path = os.path.join(curr_dir, "../../services/redis")
+            redis_bin = os.path.join(redis_path, "redis/src/redis-server")
+            redis_conf = os.path.join(redis_path, "redis.conf")
+            print(f"Starting Redis server using {redis_bin} {redis_conf} at {host}:{port}")
             proc = subprocess.Popen(
                 [redis_bin, redis_conf, "--port", str(port)],
-                stdout=subprocess.DEVNULL,
-                stderr=subprocess.DEVNULL,
+                stdout=None if verbose else subprocess.DEVNULL,
+                stderr=None if verbose else subprocess.DEVNULL,
                 shell=False,
             )
             atexit.register(proc.terminate)
-            time.sleep(1)
         self._timeout = timeout
         self._wait_interval = wait_interval
         self._verbose = verbose
+        self._stats = RedisStoreStats()
+        time.sleep(self._wait_interval)
         if self._verbose:
-            print(f"Connecting to Redis server at {host}:{port} at {datetime.now()}")
-        self._redis = redis.Redis(host=host, port=port, db=0)
+            print(f"Connecting to Redis server at {host}:{port}")
         curr_time = time.time()
+        self._redis = redis.Redis(host=host, port=port, db=0)
         while True:
             try:
                 self._redis.ping()
@@ -53,23 +94,34 @@ class RedisStore(Store):
                     print(f"Failed to ping Redis server at {host}:{port}, retrying...")
                 if time.time() - curr_time > self._timeout.total_seconds():
                     raise TimeoutError("Timeout connecting to Redis server")
-                time.sleep(1)
+                time.sleep(0.1)
         if self._verbose:
             print(f"Connected to Redis server at {datetime.now()}")
+        self._stats.startup_time = time.time() - start_time
+
+    def reset_stats(self):
+        self._stats.reset()
+
+    def dump_stats(self):
+        self._stats.dump()
 
     def dump_keys(self):
         all_keys = self._redis.keys()
-        print(f"all keys: {','.join([k.decode('utf-8') for k in all_keys])}")
+        print(f"all {self.num_keys()} keys: {','.join([k.decode('utf-8') for k in all_keys])}")
 
     def set(self, key: str, value: str):
+        start_time = time.time()
         if self._verbose:
             print(f"Setting key {key} to value {value}")
         self._redis.set(key, value)
+        self._stats.add_delay("set", time.time() - start_time)
 
     def get(self, key: str) -> bytes:
+        start_time = time.time()
         if self._verbose:
             print(f"Getting key {key}")
         start_time = time.time()
+        retry = 0
         value = b""
         while True:
             value = self._redis.get(key)
@@ -78,10 +130,15 @@ class RedisStore(Store):
             if time.time() - start_time > self._timeout.total_seconds():
                 raise TimeoutError(f"Timeout getting key {key}")
             time.sleep(self._wait_interval)
+            retry += 1
+        self._stats.add_delay("get", time.time() - start_time, retry)
         return value
 
     def add(self, key: str, value: int) -> int:
-        return self._redis.incrby(key, value)
+        start_time = time.time()
+        res = self._redis.incrby(key, value)
+        self._stats.add_delay("add", time.time() - start_time)
+        return res
 
     def compare_set(
         self,
@@ -89,20 +146,27 @@ class RedisStore(Store):
         expected_value: str,
         desired_value: str,
     ) -> bytes:
+        start_time = time.time()
         existing_value = self._redis.get(key)
+        res = None
         if existing_value is None:
             if expected_value == "":
                 self._redis.set(key, desired_value)
-                return desired_value.encode()
+                res = desired_value.encode()
             else:
-                return expected_value.encode()
+                res = expected_value.encode()
         else:
             if existing_value == expected_value:
                 self._redis.set(key, desired_value)
-            return existing_value
+            res = existing_value
+        self._stats.add_delay("compare", time.time() - start_time)
+        return res
 
     def delete_key(self, key: str) -> bool:
-        return self._redis.delete(key) > 0
+        start_time = time.time()
+        res = self._redis.delete(key) > 0
+        self._stats.add_delay("delete", time.time() - start_time)
+        return res
 
     def num_keys(self) -> int:
         count = len(self._redis.keys())
@@ -122,6 +186,8 @@ class RedisStore(Store):
         ...
 
     def wait(self, keys: List[str], timeout: Optional[timedelta]=None):
+        start_time = time.time()
+        retry = 0
         if self._verbose:
             print(f"Waiting for keys {keys}")
         eff_timeout = self._timeout if timeout is None else timeout
