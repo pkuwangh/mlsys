508d76b84db38151e1362ff4844734a820ff11d0
Hao Wang <haowan@nvidia.com>
2024-03-05 16:51:44 -0800

    [env] cleanup environment setup

--- mlsys/environments/scripts/common.sh
+++ mlsys/environments/scripts/common.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# take one argument as the message
 infoMsg() {
     echo -e "\033[32;1m${1}\033[0m"
 }
@@ -13,10 +14,30 @@ debugMsg() {
 }
 
 # take one argument as the message
-
 continueOrExit() {
-    read -p "Continue with ${1}? (y/N)" -n 1 -r
+    read -p "Continue with ${1}? (y/N) " -n 1 -r
+    echo ""
     if [[ ! $REPLY =~ ^[Yy]$ ]]; then
         exit 1
     fi
+}
+
+# check if the virtual environment is activated
+checkVenv() {
+    infoMsg "Checking python virtual environment..."
+    local IN_VENV=$( python3 -c 'import sys ; print( 0 if sys.prefix == sys.base_prefix else 1 )' )
+    if [ $IN_VENV -eq 0 ]; then
+        warnMsg "Please activate your virtual environment first."
+        exit 1
+    fi
+    # double check
+    which python3
+    continueOrExit "the above python virtualenv"
+}
+
+# check my dev envrionment
+checkDevEnv() {
+    infoMsg "Checking my dev environment..."
+    local CURR_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+    "${CURR_DIR}/../check_cuda.py"
 }
---
+++ mlsys/environments/setup.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# get current directory
+CURR_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+source "${CURR_DIR}/scripts/common.sh"
+
+# double check python virtual env
+checkVenv
+
+# install system deps
+infoMsg "Installing system dependencies..."
+sudo apt-get install libcudnn9-cuda-12 libopenblas-dev
+
+# install python modules
+infoMsg "Installing python modules..."
+pip3 install -r "${CURR_DIR}/requirements/requirements-deps.txt"
+
+# nccl!
+infoMsg "To install NCCL:"
+debugMsg " - To build on your own, follow packages/README.md"
+debugMsg " - To install directly, search with ./scripts/find_nccl.sh"
+
+# pytorch!
+infoMsg "To install PyTorch:"
+debugMsg " - To build on your own, follow packages/README.md"
+debugMsg " - To use pre-built, run"
+debugMsg "   - pip3 install -r requirements/requirements-pytorch.txt"
+debugMsg "   - ./scripts/install_pytorch_grace_nccl.sh with NCCL/UCC support for aarch64"
+debugMsg "   - ./scripts/install_pytorch_grace_plain.sh without NCCL support for aarch64"
