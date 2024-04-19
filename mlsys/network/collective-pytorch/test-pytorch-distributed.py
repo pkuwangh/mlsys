640fba79f0f9841fcccb4a81f0c58b203464076b
Hao Wang <pkuwangh@gmail.com>
2024-04-19 14:57:37 -0700

    [network] reduce, reduce-scatter, and all-reduce micro using torch

--- mlsys/network/collective-pytorch/test-pytorch-distributed.py
+++ mlsys/network/collective-pytorch/test-pytorch-distributed.py
@@ -7,6 +7,7 @@ import torch
 import torch.distributed
 
 from datetime import timedelta
+from typing import Tuple
 
 
 def get_data_size_str(data_size):
@@ -51,7 +52,7 @@ def dump_bandwidth(
     rank = torch.distributed.get_rank()
     if rank == 0:
         print(
-            f"{torch.distributed.get_backend(group)} {test_name:<10s}:"
+            f"{torch.distributed.get_backend(group)} {test_name:<14s}:"
             f"  data_size={get_data_size_str(data_size)},"
             f"  num_iters={num_iters:>4d},"
             f"  time={elapsed_time:4.1f}s,"
@@ -60,65 +61,107 @@ def dump_bandwidth(
         )
 
 
-def test_sendrecv(group, tensor, data_size, num_iters):
+def print_rank_0(line):
+    if torch.distributed.get_rank() == 0:
+        print(line)
+
+
+def get_basic_info() -> Tuple[int, int]:
     rank = torch.distributed.get_rank()
-    half_world_size = torch.distributed.get_world_size() // 2
+    world_size = torch.distributed.get_world_size()
+    return (rank, world_size)
+
+
+def start() -> float:
     torch.distributed.barrier()
-    start_time = time.time()
+    return time.time()
+
+
+def end(start_time: float, data_size: int, num_iters: int) -> Tuple[float, float]:
+    torch.distributed.barrier()
+    elapsed_time = time.time() - start_time
+    bandwidth = data_size * num_iters / elapsed_time / 1e9
+    return (elapsed_time, bandwidth)
+
+
+def test_sendrecv(group, tensor, data_size, num_iters):
+    (rank, world_size) = get_basic_info()
+    half_world_size = world_size // 2
+    start_time = start()
     for _ in range(num_iters):
         if rank < half_world_size:
             torch.distributed.send(tensor, dst=(rank + half_world_size), group=group)
         else:
             torch.distributed.recv(tensor, src=(rank - half_world_size), group=group)
-    torch.distributed.barrier()
-    end_time = time.time()
-    elapsed_time = end_time - start_time
-    bandwidth = data_size * num_iters / elapsed_time / 1e9
-    if rank == 0:
-        dump_bandwidth(group, "SendRecv", data_size, num_iters, elapsed_time, bandwidth, bandwidth)
+    (elapsed_time, bandwidth) = end(start_time, data_size, num_iters)
+    dump_bandwidth(group, "SendRecv", data_size, num_iters, elapsed_time, bandwidth, bandwidth)
 
 
 def test_gather(group, tensor, data_size, num_iters):
-    rank = torch.distributed.get_rank()
-    world_size = torch.distributed.get_world_size()
+    (rank, world_size) = get_basic_info()
     send_tensor = tensor
     if rank == 0:
         recv_tensors = [torch.empty_like(tensor) for _ in range(world_size)]
     else:
         recv_tensors = None
-    torch.distributed.barrier()
-    start_time = time.time()
+    start_time = start()
     for _ in range(num_iters):
         torch.distributed.gather(send_tensor, recv_tensors, dst=0, group=group)
-    torch.distributed.barrier()
-    end_time = time.time()
-    elapsed_time = end_time - start_time
-    bandwidth = data_size * num_iters / elapsed_time / 1e9
+    (elapsed_time, bandwidth) = end(start_time, data_size, num_iters)
     bus_bandwidth = bandwidth
-    if rank == 0:
-        dump_bandwidth(
-            group, "Gather", data_size, num_iters, elapsed_time, bandwidth, bus_bandwidth
-        )
+    dump_bandwidth(group, "Gather", data_size, num_iters, elapsed_time, bandwidth, bus_bandwidth)
 
 
 def test_all_gather(group, tensor, data_size, num_iters):
-    rank = torch.distributed.get_rank()
-    world_size = torch.distributed.get_world_size()
+    (rank, world_size) = get_basic_info()
     send_tensor = tensor
     recv_tensors = [torch.empty_like(tensor) for _ in range(world_size)]
-    torch.distributed.barrier()
-    start_time = time.time()
+    start_time = start()
     for _ in range(num_iters):
         torch.distributed.all_gather(recv_tensors, send_tensor, group=group)
-    torch.distributed.barrier()
-    end_time = time.time()
-    elapsed_time = end_time - start_time
-    bandwidth = data_size * num_iters / elapsed_time / 1e9
+    (elapsed_time, bandwidth) = end(start_time, data_size, num_iters)
     bus_bandwidth = bandwidth * (world_size - 1) / world_size
-    if rank == 0:
-        dump_bandwidth(
-            group, "Gather", data_size, num_iters, elapsed_time, bandwidth, bus_bandwidth
+    dump_bandwidth(group, "AllGather", data_size, num_iters, elapsed_time, bandwidth, bus_bandwidth)
+
+
+def test_reduce(group, tensor, data_size, num_iters):
+    (rank, world_size) = get_basic_info()
+    start_time = start()
+    for _ in range(num_iters):
+        torch.distributed.reduce(tensor, dst=0, op=torch.distributed.ReduceOp.SUM, group=group)
+    (elapsed_time, bandwidth) = end(start_time, data_size, num_iters)
+    bus_bandwidth = bandwidth
+    dump_bandwidth(group, "Reduce", data_size, num_iters, elapsed_time, bandwidth, bus_bandwidth)
+
+
+def test_reduce_scatter(group, tensor, data_size, num_iters):
+    if torch.distributed.get_backend(group) == "gloo":
+        return
+    (rank, world_size) = get_basic_info()
+    tensor_size = list(tensor.size())
+    chunk_size = [x for x in tensor_size[:-1]] + [tensor_size[-1] // world_size]
+    input = [torch.randn(chunk_size).cuda() for _ in range(world_size)]
+    output = torch.empty_like(input[0])
+    start_time = start()
+    for _ in range(num_iters):
+        torch.distributed.reduce_scatter(
+            output, input, op=torch.distributed.ReduceOp.SUM, group=group
         )
+    (elapsed_time, bandwidth) = end(start_time, data_size, num_iters)
+    bus_bandwidth = bandwidth * (world_size - 1) / world_size
+    dump_bandwidth(
+        group, "ReduceScatter", data_size, num_iters, elapsed_time, bandwidth, bus_bandwidth
+    )
+
+
+def test_all_reduce(group, tensor, data_size, num_iters):
+    (rank, world_size) = get_basic_info()
+    start_time = start()
+    for _ in range(num_iters):
+        torch.distributed.all_reduce(tensor, op=torch.distributed.ReduceOp.SUM, group=group)
+    (elapsed_time, bandwidth) = end(start_time, data_size, num_iters)
+    bus_bandwidth = bandwidth * 2 * (world_size - 1) / world_size
+    dump_bandwidth(group, "AllReduce", data_size, num_iters, elapsed_time, bandwidth, bus_bandwidth)
 
 
 def test_all(args, groups):
@@ -135,13 +178,16 @@ def test_all(args, groups):
         {"func": test_sendrecv, "data_size_scale": 1},
         {"func": test_gather, "data_size_scale": world_size},
         {"func": test_all_gather, "data_size_scale": world_size},
+        {"func": test_reduce, "data_size_scale": 1},
+        {"func": test_reduce_scatter, "data_size_scale": 1},
+        {"func": test_all_reduce, "data_size_scale": 1},
     ]
     for group_info in groups:
         group = group_info["group"]
         group_iter_scale = group_info["group_iter_scale"]
         for test_info in tests:
             for num in sorted(size_and_iters.keys()):
-                data = torch.randn(1024, num // test_info["data_size_scale"])
+                data = torch.randn(num // test_info["data_size_scale"], 1024)
                 if torch.distributed.get_backend(group) == "nccl":
                     tensor = data.cuda()
                 else:
@@ -155,6 +201,8 @@ def test_all(args, groups):
                 num_iters = max(1, int(num_iters))
                 # test
                 test_info["func"](group, tensor, data_size, num_iters)
+            print_rank_0("----")
+        print_rank_0("====")
 
 
 def main(args):
