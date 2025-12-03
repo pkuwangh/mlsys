805083af501da9e447caeba57a9459dc0cb2b80d
Hao Wang <haowan@nvidia.com>
2025-12-03 15:47:23 -0800

    [wip][vllm] add test and note for expert parallel

---
+++ mlsys/inference/vllm/quickstart/v03-expert-parallel.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+
+import os
+import nvtx
+import torch
+from loguru import logger
+from torch.cuda import cudart
+from utils import get_model_path
+from vllm import LLM, SamplingParams
+from vllm.outputs import RequestOutput
+
+
+@nvtx.annotate("setup")
+def setup() -> LLM:
+    model_path = get_model_path("Qwen/Qwen3-VL-30B-A3B-Instruct-FP8/")
+    logger.warning(f"[pid={os.getpid()}] Loading model from: {model_path}")
+    return LLM(model=model_path, enable_expert_parallel=True)
+
+
+@nvtx.annotate("run")
+def run(llm: LLM, prompts: list[str]) -> list[RequestOutput]:
+    sampling_params = SamplingParams(temperature=0.8, top_p=0.95, max_tokens=16)
+    logger.info(f"sampling_params: {sampling_params}")
+
+    logger.info("Generating text...")
+    outputs = llm.generate(prompts, sampling_params, use_tqdm=False)
+    return outputs
+
+
+if __name__ == "__main__":
+    # with TP=2, the worker will init the cuda context?
+    # torch.cuda.set_device(0)
+    # torch.zeros(1, device="cuda")
+
+    llm = setup()
+
+    prompts = [
+        "Hello, my name is",
+        "The president of the United States is",
+        "The capital of France is",
+        "The future of AI is",
+    ]
+    prompts = prompts
+    logger.info(f"Prepared {len(prompts)} prompts for generation.")
+
+    cudart().cudaProfilerStart()
+    outputs = run(llm, prompts)
+    torch.cuda.synchronize()
+    cudart().cudaProfilerStop()
+
+    for output in outputs:
+        prompt = output.prompt
+        generated_text = output.outputs[0].text
+        logger.info(f"OSL={len(output.outputs[0].token_ids)}: {prompt} -> {generated_text}")
