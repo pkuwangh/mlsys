fbd3ec6caec84d95bcf0e1394486989fa87f2315
Hao Wang <pkuwangh@gmail.com>
2026-01-16 14:37:10 -0800

    [matmul] get things running on blackwell

---
+++ mlsys/gpu/source_me_install_cu130.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+# get current directory
+CURR_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+source "${CURR_DIR}/../../scripts/common.sh"
+
+splitLine
+checkVenv
+if [ $? -ne 0 ]; then
+    return 1
+fi
+
+cleanupCondaBackEnvs
+micromamba install -y \
+    -c nvidia \
+    "nvidia::cuda-toolkit==13.0.0" \
+    -c conda-forge \
+    gcc libboost-devel openmpi-mpicxx \
+    --strict-channel-priority
+
+pip install cmake==4.1.0 ninja==1.13.0
+
+pip install black loguru ruff "huggingface_hub[cli]"
+
+# for cmake find_package
+export CUDA_HOME="${CONDA_PREFIX}"
+export CUDA_PATH="${CUDA_HOME}"
+export CUDACXX="${CUDA_HOME}/bin/nvcc"
+
+# for conda-installed CUDA, real home is under /targets/$(uname -m)-linux
+_SCATTERED_CUDA_HOME="${CUDA_HOME}/targets/$(uname -m)-linux"
+if [ ! -d "${_SCATTERED_CUDA_HOME}" ]; then
+    debugMsg "Cannot find scattered CUDA home: ${_SCATTERED_CUDA_HOME}"
+    _OLD_SCATTERED_CUDA_HOME=$_SCATTERED_CUDA_HOME
+    _SCATTERED_CUDA_HOME="${CUDA_HOME}/targets/sbsa-linux"
+    if [ ! -d "${_SCATTERED_CUDA_HOME}" ]; then
+        warnMsg "Cannot find scattered CUDA home: ${_OLD_SCATTERED_CUDA_HOME} and ${_SCATTERED_CUDA_HOME}"
+        return 1
+    fi
+fi
+infoMsg "Using scattered CUDA home: ${_SCATTERED_CUDA_HOME}"
+
+# link critical header files
+_HEADERS=("cuda.h" "cuda_runtime.h" "cuda_runtime_api.h" "device_functions.h")
+for header in "${_HEADERS[@]}"; do
+    if [ ! -f "${CUDA_HOME}/include/${header}" ]; then
+        ln -s "${_SCATTERED_CUDA_HOME}/include/${header}" "${CUDA_HOME}/include/${header}"
+    fi
+done
+
+# libcudart.so is under /targets/x86_64-linux/lib, but is linked to /lib
+# libcuda.so is under /targets/x86_64-linux/lib/stub
+export LIBRARY_PATH="${_SCATTERED_CUDA_HOME}/lib:${_SCATTERED_CUDA_HOME}/lib/stub:${LIBRARY_PATH}"
+export LD_LIBRARY_PATH="${_SCATTERED_CUDA_HOME}/lib:${_SCATTERED_CUDA_HOME}/lib/stub:${LD_LIBRARY_PATH}"
+
+pip install \
+    torch torchvision \
+    --index-url https://download.pytorch.org/whl/cu130
+
+pip install pycuda
+
+splitLine
+infoMsg "Checking gcc, nvcc"
+which gcc
+gcc --version
+which nvcc
+nvcc --version
